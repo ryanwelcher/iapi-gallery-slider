abb938a62ea3627dadc1fe1983da412c14d79d8c
Ryan Welcher <me@ryanwelcher.com>
2024-03-06 13:49:26 +0800

    Use class_list method in slides.

--- iapi-gallery-slider.php
+++ iapi-gallery-slider.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name:       Iapi Gallery Slider
+ * Plugin Name:       IAPI Gallery Slider
  * Description:       An interactive block with the Interactivity API
- * Version:           0.1.0
- * Requires at least: 6.1
+ * Version:           1.0.0
+ * Requires at least: 6.4
  * Requires PHP:      7.0
- * Author:            The WordPress Contributors
+ * Author:            Ryan Welcher
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       iapi-gallery-slider
@@ -31,18 +31,21 @@ add_action( 'init', 'iapi_gallery_slider_iapi_gallery_slider_block_init' );
 
 /**
  * Filter the render_block to add the needed directives to the inner cover blocks.
+ *
+ * @param string $block_content The content being rendered by the block.
  */
 function add_directives_to_inner_blocks( $block_content ) {
 	$allowed_blocks = array( 'wp-block-cover', 'wp-block-image', 'wp-block-media-text' );
 	$covers         = new \WP_HTML_Tag_Processor( $block_content );
 
 	while ( $covers->next_tag() ) {
-		$classes = explode( ' ', $covers->get_attribute( 'class' ) );
-
-		foreach ( $allowed_blocks as $block_class ) {
-			if ( in_array( $block_class, $classes, true ) ) {
+		// Retrieve and iterate over the classes assigned.
+		foreach ( $covers->class_list() as $class_name ) {
+			if ( in_array( $class_name, $allowed_blocks, true ) ) {
 				$covers->set_attribute( 'data-wp-interactive', '{"namespace":"iapi-gallery"}' );
 				$covers->set_attribute( 'data-wp-init', 'callbacks.initSlide' );
+				// If we find a class, we can move on - this is still not very performant as the worst case is that we loop all classes against all allowed classes. Not an issue with the tag processor, rather the code I wrote with it.
+				continue;
 			}
 		}
 	}
