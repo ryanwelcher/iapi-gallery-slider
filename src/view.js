8e9e01710320435da81e08367bae93ac91d72426
Ryan Welcher <me@ryanwelcher.com>
2024-03-19 12:21:22 -0400

    Fix some bad logic for the continuous mode.

--- src/view.js
+++ src/view.js
@@ -30,8 +30,7 @@ const { state, actions } = store( 'iapi-gallery', {
 		},
 		get imageIndex() {
 			const ctx = getContext();
-			// return ctx.slideIndexDisplay;
-			return `${ ctx.currentSlide + 1 }/${ ctx.totalSlides }`;
+			return `${ ctx.currentSlide }/${ ctx.totalSlides }`;
 		},
 		get transitionsSpeed() {
 			const ctx = getContext();
@@ -41,8 +40,9 @@ const { state, actions } = store( 'iapi-gallery', {
 	actions: {
 		prevImage: () => {
 			const ctx = getContext();
-			if ( ctx.continuous && ctx.currentSlide === 0 ) {
-				ctx.currentSlide = ctx.totalSlides - 1;
+			console.log( ctx.currentSlide );
+			if ( ctx.continuous && ctx.currentSlide === 1 ) {
+				ctx.currentSlide = ctx.totalSlides;
 				return;
 			}
 			ctx.currentSlide--;
@@ -51,9 +51,9 @@ const { state, actions } = store( 'iapi-gallery', {
 			const ctx = getContext();
 			if (
 				( ctx.continuous || ctx.autoplay ) &&
-				ctx.currentSlide === ctx.totalSlides - 1
+				ctx.currentSlide === ctx.totalSlides
 			) {
-				ctx.currentSlide = 0;
+				ctx.currentSlide = 1;
 				return;
 			}
 			ctx.currentSlide++;
@@ -111,8 +111,6 @@ const { state, actions } = store( 'iapi-gallery', {
 			// Adds the element reference to the `slides` array.
 			const { ref } = getElement();
 			ctx.slides.push( ref );
-			// ctx.totalSlides = ctx.slides.length;
-
 			// The returned function executes when the element is removed from
 			// the DOM.
 			return () => {
