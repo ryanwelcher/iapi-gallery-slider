f9faaba65173ca987f8ce74a885253dba90ebec8
Ryan Welcher <me@ryanwelcher.com>
2026-08-31 16:23:10 -0400

    Demo branch: built finished slider plus a Playground blueprint that lands on a populated demo post.

---
+++ build/render.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * PHP file to use when rendering the block type on the server to show on the front end.
+ *
+ * The following variables are exposed to the file:
+ *     $attributes (array): The block attributes.
+ *     $content (string): The block default content.
+ *     $block (WP_Block): The block instance.
+ */
+
+?>
+<div <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>
+	data-wp-interactive='iapi-gallery'
+	data-wp-init="callbacks.initSlideShow"
+	role="region"
+	aria-roledescription="carousel"
+	aria-label="Image gallery"
+	data-wp-on--focusin="actions.pauseAutoplay"
+	data-wp-on--focusout="actions.resumeAutoplay"
+>
+	<div
+		class="slider-container"
+		data-wp-style--transform="state.currentPos"
+		data-wp-on--touchstart="actions.onTouchStart"
+		data-wp-on--touchend="actions.onTouchEnd"
+	>
+		<?php echo wp_kses_post( $content ); ?>
+	</div>
+	<div class="buttons">
+		<button data-wp-on--click="actions.prevImage" data-wp-bind--disabled="state.noPrevSlide" aria-label="go to previous slide">&lt;</button>
+		<p data-wp-text="state.imageIndex"></p>
+		<button data-wp-on--click="actions.nextImage" data-wp-bind--disabled="state.noNextSlide" aria-label="go to next slide">&gt;</button>
+	</div>
+</div>
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '18d9a1a22c6faac30944', 'type' => 'module');
